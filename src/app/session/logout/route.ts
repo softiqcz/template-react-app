2fe519f2cadf4677f69ec7edf1562aeee5334e51
Martin Švejda <svejdam@icloud.com>
2026-07-05 17:01:05 +0200

    Revert "offcanvas"
    This reverts commit e5b849fdd3f63544ca61e650601b1f7bff6adff6.

--- src/app/session/logout/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-export async function POST() {
-  const response = NextResponse.json({ success: true });
-  response.cookies.set("user_info", "", { httpOnly: true, path: "/", maxAge: 0 });
-  return response;
-}
